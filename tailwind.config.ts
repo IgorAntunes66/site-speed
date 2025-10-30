2cbd3d56ca1358c431f9c9a5946b3174450c1b02
IgorAntunes66 <igorgantunes@hotmail.com>
2025-10-30 01:36:04 -0400

    chore: adicionado arquivo de configuracao do tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    // Nós precisamos preencher isso!
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
