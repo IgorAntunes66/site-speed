2ae386969431893028e08cc793758d09c2c3d4e2
IgorAntunes66 <igorgantunes@hotmail.com>
2025-11-07 17:26:54 -0400

    feat(section): inclui section 'inicial'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
-import { Button } from "@/components/ui/button"
-import Navbar from "@/components/navbar"
+import { Button } from "@/components/ui/button";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
+      <section className="relative flex h-[600px] items-center justify-center bg-[#02022E]">
+        <Navbar />
+        <div className="flex flex-wrap justify-center gap-y-6">
+          <h1 className="text-center text-6xl font-bold text-white">
+            O SISTEMA DE GESTÃO QUE IMPULSIONA O SEU NEGÓCIO
+          </h1>
+          <p className="text-white">
+            Deixe a complexidade para nós. Nossa plataforma oferece uma solução
+            administrativa completa: desde o controle de estoque e frota até a
+            gestão financeira e emissão de notas fiscais com TEF, otimizando seu
+            tempo e seus resultados.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
