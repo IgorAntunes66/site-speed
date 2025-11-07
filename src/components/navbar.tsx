5a2a7dddf4654c210c2ff2fb3a7eb8d6e51fecb4
IgorAntunes66 <igorgantunes@hotmail.com>
2025-11-07 10:30:42 -0400

    refactor(navbar): utiliza cva para armazenar os estilos em uma constante

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,45 +7,74 @@ import {
   NavigationMenuList,
   //NavigationMenuTrigger,
   //NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
+} from "@/components/ui/navigation-menu";
 
-import {Button} from "@/components/ui/button"
-import Image from "next/image"
+import { Button } from "@/components/ui/button";
+import { cva } from "class-variance-authority";
+import Image from "next/image";
+
+const headerStyles = cva("flex items-center justify-around px-56 py-2");
+const navLinkStyle = cva(
+  "transition-transform duration-200 text-[14px] font-medium hover:scale-115 text-gray-300 hover:font-bold hover:text-white inline-block",
+);
+const buttonStyle = cva(
+  "bg-white text-gray-900 shadow-2xl transition-transform duration-200 hover:scale-105 hover:bg-gray-100",
+);
 
 export default function Navbar() {
-  const navLinkStyle = "transition-transform duration-200 text-[14px] hover:scale-115 text-gray-300 hover:font-medium hover:text-white inline-block"
   return (
     <div className="bg-[#02022E]">
-      <header className="flex justify-around items-center px-56 py-2">
+      <header className={headerStyles()}>
         <div className="">
-          <Image src="/logo_completa_branca.svg" alt="Logo" className="w-58 h-auto " width={0} height={0}/>
+          <Image
+            src="/logo_completa_branca.svg"
+            alt="Logo"
+            className="w-58 h-auto"
+            width={500}
+            height={500}
+          />
         </div>
         <NavigationMenu>
           <NavigationMenuList className="gap-x-16">
             <NavigationMenuItem>
-              <NavigationMenuLink href="/" className={navLinkStyle}>INÍCIO</NavigationMenuLink>
+              <NavigationMenuLink href="/" className={navLinkStyle()}>
+                INÍCIO
+              </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink href="#sobre" className={navLinkStyle}>SOBRE</NavigationMenuLink>
+              <NavigationMenuLink href="#sobre" className={navLinkStyle()}>
+                SOBRE
+              </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink href="#aplicativos" className={navLinkStyle}>APLICATIVOS</NavigationMenuLink>
+              <NavigationMenuLink
+                href="#aplicativos"
+                className={navLinkStyle()}
+              >
+                APLICATIVOS
+              </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink href="#servicos" className={navLinkStyle}>SERVIÇOS</NavigationMenuLink>
+              <NavigationMenuLink href="#servicos" className={navLinkStyle()}>
+                SERVIÇOS
+              </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink href="#clientes" className={navLinkStyle}>CLIENTES</NavigationMenuLink>
+              <NavigationMenuLink href="#clientes" className={navLinkStyle()}>
+                CLIENTES
+              </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink href="#contato" className={navLinkStyle}>CONTATO</NavigationMenuLink>
+              <NavigationMenuLink href="#contato" className={navLinkStyle()}>
+                CONTATO
+              </NavigationMenuLink>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
         <div>
-          <Button className="transition-transform duration-200 hover:scale-105 bg-white text-gray-900 shadow-2xl hover:bg-gray-100">Login</Button>
+          <Button className={buttonStyle()}>Login</Button>
         </div>
       </header>
     </div>
-  )
+  );
 }
