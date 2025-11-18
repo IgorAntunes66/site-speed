4a24f31a75a196ad15a718f0ecd704172b4e2cae
IgorAntunes66 <igorgantunes@hotmail.com>
2025-11-18 17:24:13 -0400

    feat(section): inclui section 'sobre' e divisor para as section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Navbar from "@/components/navbar";
 import HeroBubbles from "@/components/heroBubbles"
+import Waves from "@/components/waves";
 
 export default function Home() {
   return (
     <div>
-      <section className="relative overflow-hidden flex h-auto md:h-[600px] pt-28 pb-12 md:pt-0 md:pb-0 md:items-center md:justify-center bg-linear-to-r from-[#020246] to-[#080868]">
+      <section className="relative overflow-hidden flex h-auto md:h-[600px] pt-28 pb-12 md:pt-0 md:pb-0 md:items-center md:justify-center bg-radial from-[#272727] to-[#060606]">
         <Navbar />
         <HeroBubbles/>
         <div className="flex flex-wrap justify-center gap-y-6 max-w-4xl px-8 md:px-0 relative z-10">
@@ -21,8 +22,18 @@ export default function Home() {
           <Button className="transition-transform duration-200 text-white bg-transparent border-2 h-15 w-40 rounded-full shadow-md shadow-indigo-500/30 hover:scale-102 hover:bg-white hover:text-[#020246]">Quero conhecer</Button>
         </div>
       </section>
-      <section id="sobre">
-
+      <section id="sobre" className="relative h-auto md:h-[600px] pl-6 pr-6 mt-[-70px] z-50">
+        <Waves/>
+        <div className="flex flex-col h-[600px] pb-18 pt-20">
+          <h1 className="text-sm text-center scroll-m-20 border-b pb-2 font-semibold tracking-tight first:mt-0">
+            GESTÃO INTELIGENTE QUE GERA RESULTADOS REAIS
+          </h1>
+          <p className="text-sm pt-6 text-center">Somos a Speed System e, desde 1994, transformamos dados em resultados. Nosso principal produto,
+            o SiFrA (Sistema Financeiro Administrativo), é uma solução de gestão completa (ERP) que atende todas as pontas do seu negócio. 
+            Ele centraliza suas operações mais importantes, permitindo o gerenciamento do caixa diário, a emissão de notas fiscais, o controle total do estoque,
+              a geração do EFD contábil, além de ter integração para recebimentos com o TEF (Transferencia eletronica de fundos). Com mais de 50 relatórios, o SiFrA coloca o poder 
+              de decisão nas suas mãos, com a confiança de um sistema validado por décadas de mercado.</p>
+        </div>
       </section>
     </div>
   );
